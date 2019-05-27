f66c9957f7823fe055b9f128b5342a0c600d7fde
Gonzalo D'Elia <gonzalo.n.delia@gmail.com>
2019-05-27 11:19:17 -0300

    Moved base url for marvel api to .env file

--- src/Api/Fetch.js
+++ src/Api/Fetch.js
@@ -1,5 +1,10 @@
+// If the API worked fine, this file would not exists.
+// MarvelApi would be the encapsulated service that handles all requests
+// however, due to the need of mocking (or not easily) the results
+// I created this file. However, it does its functionality and that's all
+// It is now a beautiful code to maintain :) 
 import mock from './ListMock';
-const BaseMarvelUrl = 'https://gateway.marvel.com/v1/public';
+const BaseMarvelUrl = process.env.REACT_APP_MARVEL_API_URL;
 const apiKeys = process.env.REACT_APP_MARVEL_API_KEY;
 
 const getJsonParams = uri => {
@@ -13,6 +18,7 @@ const getJsonParams = uri => {
     }, {});
 };
 
+// show this message because user might have an api key, but forgot to configure it
 const warnAboutMocking = () => !isTestingEnv()
 && console.warn('Mocking Marvel Api request with a harcoded json. Perhaps you forgot to configure your key?');
 

5687b4c789560cf554dd3f672726968d31b57d48
Gonzalo D'Elia <gonzalo.n.delia@gmail.com>
2019-05-27 10:43:36 -0300

    Updates

---
+++ precache-manifest.37dc720ae825aa738c9cd7a047ac8b26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b8c0e94dd056d288fa779ef4673141",
+    "url": "/marvel-explorer/index.html"
+  },
+  {
+    "revision": "0cd968aee0ef261a7cba",
+    "url": "/marvel-explorer/static/css/main.7227d080.chunk.css"
+  },
+  {
+    "revision": "84bdae48ac638b21f293",
+    "url": "/marvel-explorer/static/js/2.011cc290.chunk.js"
+  },
+  {
+    "revision": "0cd968aee0ef261a7cba",
+    "url": "/marvel-explorer/static/js/main.163f42fa.chunk.js"
+  },
+  {
+    "revision": "8bb820309f0a4d4723ab",
+    "url": "/marvel-explorer/static/js/runtime~main.47842ff8.js"
+  }
+]);
